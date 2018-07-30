121819acbca5db8e4c8779ed1a76711294d101e5
Corina <v-corgum@microsoft.com>
2018-07-30 15:37:42 -0700

    repository push

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=v-corgum-acwidth-Web-Deploy.pubxml -p:Password=TaPartr8q6NWJ4BWns6pJqQsoMnYSbwbHHr3vGkwQKwKM4i33LrsCLkmC7Di
+
