e3bee09dacb93e71c60e64bf2bd6a12319e3b919
Corina <v-corgum@microsoft.com>
2018-08-23 11:11:24 -0700

    return to echo bot

--- Dialogs/EchoDialog.cs
+++ Dialogs/EchoDialog.cs
@@ -33,46 +33,9 @@ namespace Microsoft.Bot.Sample.SimpleEchoBot
             }
             else
             {
-                Activity replyToConversation = message.CreateReply("Should go to conversation, in carousel format");
-                replyToConversation.AttachmentLayout = AttachmentLayoutTypes.Carousel;
-                replyToConversation.Attachments = new List<Attachment>();
-                
-                Dictionary<string, string> cardContentList = new Dictionary<string, string>();
-                cardContentList.Add("PigLatin", "https://<ImageUrl1>");
-                cardContentList.Add("Pork Shoulder", "https://<ImageUrl2>");
-                cardContentList.Add("Bacon", "https://<ImageUrl3>");
-                
-                foreach(KeyValuePair<string, string> cardContent in cardContentList)
-                {
-                    List<CardImage> cardImages = new List<CardImage>();
-                    cardImages.Add(new CardImage(url:cardContent.Value ));
-                
-                    List<CardAction> cardButtons = new List<CardAction>();
-                
-                    CardAction plButton = new CardAction()
-                    {
-                        Value = $"https://en.wikipedia.org/wiki/{cardContent.Key}",
-                        Type = "openUrl",
-                        Title = "WikiPedia Page"
-                    };
-                
-                    cardButtons.Add(plButton);
-                
-                    HeroCard plCard = new HeroCard()
-                    {
-                        Title = $"I'm a hero card about {cardContent.Key}",
-                        Subtitle = $"{cardContent.Key} Wikipedia Page",
-                        Images = cardImages,
-                        Buttons = cardButtons
-                    };
-                
-                    Attachment plAttachment = plCard.ToAttachment();
-                    replyToConversation.Attachments.Add(plAttachment);
-                }
-                
-                var reply = await connector.Conversations.SendToConversationAsync(replyToConversation);
-                context.Wait(MessageReceivedAsync);
-            }
+				await context.PostAsync($"{this.count++}: You said {message.Text}");
+				context.Wait(MessageReceivedAsync);
+			}
         }
 
         public async Task AfterResetAsync(IDialogContext context, IAwaitable<bool> argument)
