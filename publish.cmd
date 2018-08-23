d9d05b50677d2d0f2b14c5cfa69f165b873cf12f
Corina <v-corgum@microsoft.com>
2018-08-23 11:06:20 -0700

    first commit

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=vcorgumherocardbot-Web-Deploy.pubxml -p:Password=1Pp2jXaafNnqgjo9kLfHcv25g8RMmDXJtrqj3ZMHga5wTsnYZQ0gStRHtXEz
+
