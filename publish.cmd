fb69c25412ed1b4aee8a4dc75f97681cb094ca8f
Corina <v-corgum@microsoft.com>
2018-09-21 12:36:28 -0700

    initial commit

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild EchoBotWithCounter.sln -p:DeployOnBuild=true -p:PublishProfile=v-corgumignitebb1-Web-Deploy.pubxml -p:Password=xGhvmrCotG6TYAqGY4eXbTWakiwvAg7zZmTkvQ6yt7uZEaA8S2jYNN4KiJ3a
+
