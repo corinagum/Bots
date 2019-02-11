5da48d411cb2b5068d459308aae67224c44c8c91
Corina <v-corgum@microsoft.com>
2019-02-11 10:42:33 -0800

    Initial commit

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=corinagum-wc1566-Web-Deploy.pubxml -p:Password=4SNXyFsFNiKHwWGDhwYw5rFY7NglEY5JrvDae8Go8Z6wzciue1ZfPYLpPsFl
+
